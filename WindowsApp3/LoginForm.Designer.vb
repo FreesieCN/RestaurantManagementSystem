b42c6f33c7578f4a428291c00b322517cf7bb257
李志远 <2307399313@qq.com>
2024-08-30 23:51:18 +0800

    LoginForm外观完成留档 v0.0.1

--- WindowsApp3/LoginForm.Designer.vb
+++ WindowsApp3/LoginForm.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class LoginForm
+Partial Class lb_LoginForm
     Inherits System.Windows.Forms.Form
 
     'Form 重写 Dispose，以清理组件列表。
@@ -23,15 +23,21 @@ Partial Class LoginForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LoginForm))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(lb_LoginForm))
         Me.RMS_DataSet = New WindowsApp3.RMS_DataSet()
         Me.管理员信息BindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.管理员信息TableAdapter = New WindowsApp3.RMS_DataSetTableAdapters.管理员信息TableAdapter()
         Me.TableAdapterManager = New WindowsApp3.RMS_DataSetTableAdapters.TableAdapterManager()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.bt_Login = New System.Windows.Forms.Button()
         Me.Title = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
+        Me.UserName = New System.Windows.Forms.Label()
+        Me.lb_PWD = New System.Windows.Forms.Label()
+        Me.bt_Quit = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lb_ErrorInfo_0 = New System.Windows.Forms.Label()
+        Me.lb_ErrorInfo_1 = New System.Windows.Forms.Label()
+        Me.lb_ErrorInfo_2 = New System.Windows.Forms.Label()
         CType(Me.RMS_DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.管理员信息BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -60,40 +66,83 @@ Partial Class LoginForm
         Me.TableAdapterManager.菜单信息TableAdapter = Nothing
         Me.TableAdapterManager.雇员信息TableAdapter = Nothing
         '
-        'Button1
+        'bt_Login
         '
-        resources.ApplyResources(Me.Button1, "Button1")
-        Me.Button1.Name = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.bt_Login.BackColor = System.Drawing.SystemColors.ControlLight
+        resources.ApplyResources(Me.bt_Login, "bt_Login")
+        Me.bt_Login.Name = "bt_Login"
+        Me.bt_Login.UseVisualStyleBackColor = False
         '
         'Title
         '
         resources.ApplyResources(Me.Title, "Title")
+        Me.Title.BackColor = System.Drawing.SystemColors.InactiveCaption
         Me.Title.Name = "Title"
         '
-        'Label2
+        'UserName
         '
-        resources.ApplyResources(Me.Label2, "Label2")
-        Me.Label2.Name = "Label2"
+        resources.ApplyResources(Me.UserName, "UserName")
+        Me.UserName.Name = "UserName"
         '
-        'Label3
+        'lb_PWD
         '
-        resources.ApplyResources(Me.Label3, "Label3")
-        Me.Label3.Name = "Label3"
+        resources.ApplyResources(Me.lb_PWD, "lb_PWD")
+        Me.lb_PWD.Name = "lb_PWD"
         '
-        'LoginForm
+        'bt_Quit
+        '
+        Me.bt_Quit.BackColor = System.Drawing.SystemColors.ControlLight
+        resources.ApplyResources(Me.bt_Quit, "bt_Quit")
+        Me.bt_Quit.Name = "bt_Quit"
+        Me.bt_Quit.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
+        Me.TextBox1.Name = "TextBox1"
+        '
+        'TextBox2
+        '
+        resources.ApplyResources(Me.TextBox2, "TextBox2")
+        Me.TextBox2.Name = "TextBox2"
+        '
+        'lb_ErrorInfo_0
+        '
+        resources.ApplyResources(Me.lb_ErrorInfo_0, "lb_ErrorInfo_0")
+        Me.lb_ErrorInfo_0.ForeColor = System.Drawing.Color.Red
+        Me.lb_ErrorInfo_0.Name = "lb_ErrorInfo_0"
+        '
+        'lb_ErrorInfo_1
+        '
+        resources.ApplyResources(Me.lb_ErrorInfo_1, "lb_ErrorInfo_1")
+        Me.lb_ErrorInfo_1.ForeColor = System.Drawing.Color.Red
+        Me.lb_ErrorInfo_1.Name = "lb_ErrorInfo_1"
+        '
+        'lb_ErrorInfo_2
+        '
+        resources.ApplyResources(Me.lb_ErrorInfo_2, "lb_ErrorInfo_2")
+        Me.lb_ErrorInfo_2.ForeColor = System.Drawing.Color.Red
+        Me.lb_ErrorInfo_2.Name = "lb_ErrorInfo_2"
+        '
+        'lb_LoginForm
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.Window
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Controls.Add(Me.lb_ErrorInfo_1)
+        Me.Controls.Add(Me.lb_ErrorInfo_0)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.bt_Quit)
+        Me.Controls.Add(Me.lb_PWD)
+        Me.Controls.Add(Me.UserName)
         Me.Controls.Add(Me.Title)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.bt_Login)
+        Me.Controls.Add(Me.lb_ErrorInfo_2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "LoginForm"
+        Me.Name = "lb_LoginForm"
         CType(Me.RMS_DataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.管理员信息BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -105,8 +154,14 @@ Partial Class LoginForm
     Friend WithEvents 管理员信息BindingSource As BindingSource
     Friend WithEvents 管理员信息TableAdapter As RMS_DataSetTableAdapters.管理员信息TableAdapter
     Friend WithEvents TableAdapterManager As RMS_DataSetTableAdapters.TableAdapterManager
-    Friend WithEvents Button1 As Button
+    Friend WithEvents bt_Login As Button
     Friend WithEvents Title As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
+    Friend WithEvents UserName As Label
+    Friend WithEvents lb_PWD As Label
+    Friend WithEvents bt_Quit As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents lb_ErrorInfo_0 As Label
+    Friend WithEvents lb_ErrorInfo_1 As Label
+    Friend WithEvents lb_ErrorInfo_2 As Label
 End Class
--- WindowsApp3/LoginForm.vb
+++ WindowsApp3/LoginForm.vb
@@ -1,7 +1,11 @@
-﻿Public Class LoginForm
+﻿Public Class lb_LoginForm
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: 这行代码将数据加载到表“RMS_DataSet.管理员信息”中。您可以根据需要移动或移除它。
         Me.管理员信息TableAdapter.Fill(Me.RMS_DataSet.管理员信息)
+
+    End Sub
+
+    Private Sub bt_Quit_Click(sender As Object, e As EventArgs) Handles bt_Quit.Click
 
     End Sub
 End Class
